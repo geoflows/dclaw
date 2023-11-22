3ebe1a1ffea7d92479f10df511968aaa121bf947
David George <dgeorge@uw.edu>
2023-11-22 08:15:58 -0800

    WIP: src term overhaul

--- src/2d/dig/old/src2-5.x-old.f90
+++ src/2d/dig/old/src2-5.x-old.f90
@@ -17,84 +17,103 @@
       double precision, intent(inout) :: aux(maux,1-mbc:mx+mbc,1-mbc:my+mbc)
 
       !local
-      real(kind=8) :: gmod,h,hu,hv,hm,u,v,m,p,phi,kappa,S,rho,tanpsi
-      real(kind=8) :: D,tau,sigbed,kperm,compress,chi,coeff,tol
-      real(kind=8) :: vnorm,hvnorm,theta,dtheta,w,tau_enhanced,hvnorm0
-      real(kind=8) :: shear,sigebar,chitanh01,rho_fp,seg
-      real(kind=8) :: b_xx,b_yy,b_xy,gacc,beta
+      real(kind=8) :: gmod,h,hu,hv,hm,u,v,m,p,phi,kappa,S,rho,tanpsi,dti
+      real(kind=8) :: D,tau,sigbed,kperm,compress,pm,coeff,tol
+      real(kind=8) :: vnorm,hvnorm,theta,dtheta,w,taucf,fsphi,hvnorm0
+      real(kind=8) :: shear,sigebar,pmtanh01,rho_fp,seg
+      real(kind=8) :: b_xx,b_yy,b_xy,chi,beta
       real(kind=8) :: t1bot,t2top,beta2,dh,rho2,prat,b_x,b_y,dbdv
       real(kind=8) :: vlow,m2,vreg,slopebound
       real(kind=8) :: b_eroded,b_remaining,dtcoeff
       real(kind=8) :: gamma,zeta,krate,p_eq,p_litho,p_hydro,dgamma
 
       integer :: i,j,ii,jj,jjend,icount,curvature
+      logical :: ent
 
 
       ! check for NANs in solution:
       call check4nans(meqn,mbc,mx,my,q,t,2)
 
+      gmod=grav
       coeff = manning_coefficient(1) ! Current implementation of friction has manning as an array 
       ! take the first element for now. If only one value is provided to geo_data.manning_coefficient 
       ! it will be manning_coefficient(1)
       ! DIG: FIX.
 
       tol = dry_tolerance !# to prevent divide by zero in gamma
-      curvature = 0 !add friction due to curvature acceleration KRB: why is this hardcoded to 0? DLG: could default to zero from setrun.
+      curvature = 0 !add friction due to curvature acceleration KRB: why is this hardcoded to 0?
+      !write(*,*) 'src:init,value',p_initialized,init_pmin_ratio
+      if (entrainment>0) then
+         ent = .true.
+      else
+         ent = .false.
+      endif
 
-      gmod=grav !for bed-normal direction gravity
       do i=1-mbc+1,mx+mbc-1
          do j=1-mbc+1,my+mbc-1
             theta = 0.d0
             dtheta = 0.d0
             if (bed_normal==1) then
-               theta = aux(ia_theta,i,j)
+               theta = aux(i_theta,i,j)
                gmod = grav*cos(theta)
-               dtheta = -(aux(ia_theta,i+1,j) - theta)/dx
+               dtheta = -(aux(i_theta,i+1,j) - theta)/dx
             endif
 
+            !call admissibleq(q(1,i,j),q(2,i,j),q(3,i,j),q(4,i,j),q(5,i,j),u,v,m,theta)
             h = q(1,i,j)
             if (h<=dry_tolerance) cycle
             hu = q(2,i,j)
             hv = q(3,i,j)
             hm = q(4,i,j)
             p =  q(5,i,j)
-            phi = aux(ia_phi,i,j)
-            chi = q(iq_seg,i,j)/h
-            chi = max(0.0,chi)
-            chi = min(1.0,chi)
+            phi = aux(i_phi,i,j)
+            pm = q(6,i,j)/h
+            pm = max(0.0,pm)
+            pm = min(1.0,pm)
+            fsphi = aux(i_fsphi,i,j)
+
+            jjend = 1
+            dti = dt!/real(jjend,kind=8)
+            !do jj=1,jjend
 
             call admissibleq(h,hu,hv,hm,p,u,v,m,theta)
-            call auxeval(h,u,v,m,p,phi,theta,kappa,S,rho,tanpsi,D,tau,sigbed,kperm,compress,chi)
+            !integrate momentum source term
+            call auxeval(h,u,v,m,p,phi,theta,kappa,S,rho,tanpsi,D,tau,sigbed,kperm,compress,pm)
+
+            !tau = max(tau*(1.0-fsphi),0.0)
 
             vnorm = sqrt(u**2 + v**2)
             hvnorm = sqrt(hu**2 + hv**2)
             hvnorm0 = hvnorm
 
-            !integrate dynamic friction !DIG: TO DO - move dynamic friction to Riemann solver
-            hvnorm = dmax1(0.d0,hvnorm - dt*tau/rho)
-            hvnorm = hvnorm*exp(-(1.d0-m)*2.0d0*mu*dt/(rho*h**2))
-            !hvnorm = hvnorm*exp(-(1.d0-m)*2.0d0*0.1*dt/(rho*h**2.0))
+            !integrate friction
+            hvnorm = dmax1(0.d0,hvnorm - dti*tau/rho)
+            hvnorm = hvnorm*exp(-(1.d0-m)*2.0d0*mu*dti/(rho*h**2))
+            !hvnorm = hvnorm*exp(-(1.d0-m)*2.0d0*0.1*dti/(rho*h**2.0))
             if (hvnorm<1.d-16) hvnorm = 0.d0
+
 
             if (hvnorm>0.d0.and.curvature==1) then
                b_xx=(aux(1,i+1,j)-2.d0*aux(1,i,j)+aux(1,i-1,j))/(dx**2)
                b_yy=(aux(1,i,j+1)-2.d0*aux(1,i,j)+aux(1,i,j-1))/(dy**2)
                b_xy=(aux(1,i+1,j+1)-aux(1,i-1,j+1) -aux(1,i+1,j-1)+aux(1,i-1,j-1))/(4.0*dx*dy)
-               gacc = max((u**2*b_xx + v**2*b_yy + 2.0*u*v*b_xy + u**2*dtheta,0.d0)/gmod !currently only consider enhancement of g, not reduction (i.e. over a hump)
-               tau_enhanced = gacc*tau
-               hvnorm = dmax1(0.d0,hvnorm - dt*tau_enhanced/rho)
+               chi = (u**2*b_xx + v**2*b_yy + 2.0*u*v*b_xy)/gmod
+               chi = max(chi,-1.d0)
+               taucf = chi*tau
+               hvnorm = dmax1(0.d0,hvnorm - dti*taucf/rho)
+               taucf = u**2*dtheta*tau/gmod
+               hvnorm = dmax1(0.d0,hvnorm - dti*taucf/rho)
             endif
 
-            if (hvnorm0>0.d0) then ! only sets direction of (hu,hv) to be the same as before friction
+            if (hvnorm0>0.d0) then
                hu = hvnorm*hu/hvnorm0
                hv = hvnorm*hv/hvnorm0
-               !else hv,hv already = 0.0
             endif
 
             if (p_initialized==0) cycle
 
             call admissibleq(h,hu,hv,hm,p,u,v,m,theta)
-            call auxeval(h,u,v,m,p,phi,theta,kappa,S,rho,tanpsi,D,tau,sigbed,kperm,compress,chi)
+            call auxeval(h,u,v,m,p,phi,theta,kappa,S,rho,tanpsi,D,tau,sigbed,kperm,compress,pm)
 
             
             vnorm = sqrt(u**2 + v**2)
@@ -103,26 +122,26 @@
             sigebar = rho*gmod*h - p + sigma_0
             shear = 2.d0*vnorm/h
             krate = 1.5d0*shear*m*tanpsi/alpha
-            !sigebar = sigebar*exp(krate*dt)
+            !sigebar = sigebar*exp(krate*dti)
             !p = rho*gmod*h + sigma_0 - sigebar
             if (compress<1.d15) then !elasticity is = 0.0 but compress is given 1d16 in auxeval
-               p = p - dt*3.d0*vnorm*tanpsi/(h*compress)
+               p = p - dti*3.d0*vnorm*tanpsi/(h*compress)
             endif
 
             !call admissibleq(h,hu,hv,hm,p,u,v,m,theta)
-            !call auxeval(h,u,v,m,p,phi,theta,kappa,S,rho,tanpsi,D,tau,sigbed,kperm,compress,chi)
+            !call auxeval(h,u,v,m,p,phi,theta,kappa,S,rho,tanpsi,D,tau,sigbed,kperm,compress,pm)
             if (dabs(alpha_seg-1.d0)<1.d-6) then
          		seg = 0.d0
                rho_fp = rho_f
-               chitanh01=0.d0
+               pmtanh01=0.d0
 
       		else
          		seg = 1.d0
-               call calc_chitanh(chi,seg,chitanh01)
-               rho_fp = max(0.d0,(1.d0-chitanh01))*rho_f
+               call calc_pmtanh(pm,seg,pmtanh01)
+               rho_fp = max(0.d0,(1.d0-pmtanh01))*rho_f
       		endif
-            !chitanh01 = seg*(0.5*(tanh(20.0*(chi-0.80))+1.0))
-            !chitanh01 = seg*(0.5*(tanh(40.0*(chi-0.90))+1.0))
+            !pmtanh01 = seg*(0.5*(tanh(20.0*(pm-0.80))+1.0))
+            !pmtanh01 = seg*(0.5*(tanh(40.0*(pm-0.90))+1.0))
             
             !integrate pressure relaxation
             !if (compress<1.d15) then !elasticity is = 0.0 but compress is given 1d16 in auxeval
@@ -140,43 +159,43 @@
             !else
             !   p_eq = p_hydro
             !endif
-            !if (abs(chi-.5)>.49) then
-            !chitanh01 = 0.5*(tanh(20.0*(chi-0.80))+1.0)
-            p_eq = p_hydro !*(1.0-chitanh01)
+            !if (abs(pm-.5)>.49) then
+            !pmtanh01 = 0.5*(tanh(20.0*(pm-0.80))+1.0)
+            p_eq = p_hydro !*(1.0-pmtanh01)
             !p_eq = max(p_eq,0.0)
             !p_eq = min(p_eq,p_litho)
 
-            p = p_eq + (p-p_eq)*exp(krate*dt)
+            p = p_eq + (p-p_eq)*exp(krate*dti)
 
 
             call admissibleq(h,hu,hv,hm,p,u,v,m,theta)
-            call auxeval(h,u,v,m,p,phi,theta,kappa,S,rho,tanpsi,D,tau,sigbed,kperm,compress,chi)
+            call auxeval(h,u,v,m,p,phi,theta,kappa,S,rho,tanpsi,D,tau,sigbed,kperm,compress,pm)
             
 
             krate = D*(rho-rho_fp)/rho
-            hu = hu*exp(dt*krate/h)
-            hv = hv*exp(dt*krate/h)
-            hm = hm*exp(-dt*D*rho_fp/(h*rho))
-            h = h + krate*dt
+            hu = hu*exp(dti*krate/h)
+            hv = hv*exp(dti*krate/h)
+            hm = hm*exp(-dti*D*rho_fp/(h*rho))
+            h = h + krate*dti
 
             !enddo
 
             call admissibleq(h,hu,hv,hm,p,u,v,m,theta)
-            call auxeval(h,u,v,m,p,phi,theta,kappa,S,rho,tanpsi,D,tau,sigbed,kperm,compress,chi)
+            call auxeval(h,u,v,m,p,phi,theta,kappa,S,rho,tanpsi,D,tau,sigbed,kperm,compress,pm)
 
             !======================mass entrainment===========================
             
             vnorm = sqrt(u**2 + v**2)
             vlow = 0.1d0
 
-            if (ent.and.vnorm.gt.vlow.and.(aux(ia_theta,i,j)>0.d0)) then
+            if (ent.and.vnorm.gt.vlow.and.(aux(i_theta,i,j)>0.d0)) then
                b_x = (aux(1,i+1,j)+q(7,i+1,j)-aux(1,i-1,j)-q(7,i-1,j))/(2.d0*dx)
                b_y = (aux(1,i,j+1)+q(7,i,j+1)-aux(1,i,j-1)-q(7,i,j-1))/(2.d0*dy)
                dbdv = (u*b_x+v*b_y)/vnorm
                slopebound = 1.d10
                b_eroded = q(7,i,j)
-               if (dbdv<slopebound.and.b_eroded<aux(ia_theta,i,j)) then
-                  b_remaining = aux(ia_theta,i,j)-b_eroded
+               if (dbdv<slopebound.and.b_eroded<aux(i_theta,i,j)) then
+                  b_remaining = aux(i_theta,i,j)-b_eroded
                   m2 = 0.6d0
                   rho2 = m2*2700.d0 + (1.d0-m2)*1000.d0
                   beta2 = 0.66d0
@@ -192,21 +211,21 @@
                   t2top = min(t1bot,(1.d0-beta*entrainment_rate)*(tau))
                   !write(*,*) 't2top',t2top
                   prat = p/(rho*h)
-                  !dh = dt*(t1bot-t2top)/(beta2*tanh(vnorm+1.d-2)*rho2)
+                  !dh = dti*(t1bot-t2top)/(beta2*tanh(vnorm+1.d-2)*rho2)
                   vreg = ((vnorm-vlow)**2/((vnorm-vlow)**2+1.d0))
-                  dtcoeff = entrainment_rate*dt*vreg/(beta2*(vnorm+vlow)*rho2)
+                  dtcoeff = entrainment_rate*dti*vreg/(beta2*(vnorm+vlow)*rho2)
                   !dh = dtcoeff*t1bot/(1.d0 + dtcoeff*tan(phi))
                   dh = dtcoeff*(t1bot-t2top)
-                  dh = entrainment_rate*dt*(t1bot-t2top)/(rho2*beta2*vnorm)
+                  dh = entrainment_rate*dti*(t1bot-t2top)/(rho2*beta2*vnorm)
                   !write(*,*) 'dh',dh
-                  !write(*,*) 'dh/dt', dh/dt
+                  !write(*,*) 'dh/dti', dh/dti
                   dh = min(dh,b_remaining)
                   h = h + dh
                   hm = hm + dh*m2
                   q(7,i,j) = q(7,i,j) + dh
 
                   call admissibleq(h,hu,hv,hm,p,u,v,m,theta)
-                  call auxeval(h,u,v,m,p,phi,theta,kappa,S,rho,tanpsi,D,tau,sigbed,kperm,compress,chi)
+                  call auxeval(h,u,v,m,p,phi,theta,kappa,S,rho,tanpsi,D,tau,sigbed,kperm,compress,pm)
                   p = prat*rho*h
                   call admissibleq(h,hu,hv,hm,p,u,v,m,theta)
                endif
@@ -218,7 +237,7 @@
             q(3,i,j) = hv
             q(4,i,j) = hm
             q(5,i,j) = p
-            q(6,i,j) = chi*h
+            q(6,i,j) = pm*h
 
          enddo
       enddo
@@ -230,6 +249,8 @@
       if (coeff>0.d0.and.friction_depth>0.d0) then
          do i=1,mx
             do j=1,my
+
+               if (bed_normal==1) gmod = grav*cos(aux(i_theta,i,j))
                   h=q(1, i,j)
                if (h<=friction_depth) then
                  !# apply friction source term only in shallower water
@@ -237,16 +258,16 @@
                   hv=q(3,i,j)
                   hm = q(4,i,j)
                   p =  q(5,i,j)
-                  phi = aux(ia_phi,i,j)
-                  theta = aux(ia_theta,i,j)
+                  phi = aux(i_phi,i,j)
+                  theta = aux(i_theta,i,j)
                   call admissibleq(h,hu,hv,hm,p,u,v,m,theta)
                   if (h<dry_tolerance) cycle
-                  chi = q(6,i,j)/h
-                  chi = max(0.0,chi)
-                  chi = min(1.0,chi)
-                  call auxeval(h,u,v,m,p,phi,theta,kappa,S,rho,tanpsi,D,tau,sigbed,kperm,compress,chi)
+                  pm = q(6,i,j)/h
+                  pm = max(0.0,pm)
+                  pm = min(1.0,pm)
+                  call auxeval(h,u,v,m,p,phi,theta,kappa,S,rho,tanpsi,D,tau,sigbed,kperm,compress,pm)
 
-                  if (h.lt.dry_tolerance) then
+                  if (h.lt.tol) then
                      q(2,i,j)=0.d0
                      q(3,i,j)=0.d0
                   else
