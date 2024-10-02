25104065fed31c2ee64668fc64791d915bf7b678
Barnhart, Katherine (Katy) Ruth <krbarnhart@usgs.gov>
2024-10-02 17:05:05 -0600

    version of flag2refine that compiles

--- src/2d/dig/flag2refine2.f90
+++ src/2d/dig/flag2refine2.f90
@@ -2,7 +2,7 @@
 !
 ! User routine to control flagging of points for refinement.
 !
-! Specific for GeoClaw for tsunami applications and related problems
+! Specific for D-Claw
 !
 !
 !    q   = grid values including ghost cells (bndry vals at specified
@@ -23,7 +23,7 @@ subroutine flag2refine2(mx,my,mbc,mbuff,meqn,maux,xlower,ylower,dx,dy,t,level, &
                        tolsp,q,aux,amrflags)
 
     use amr_module, only: mxnest, t0, DOFLAG, UNSET
-    use amr_module, only: lfine, lstart, node, rnode  ! DIG
+    use amr_module, only: lfine, lstart, node, rnode
     use amr_module, only: cornxlo, cornylo, levelptr, mxnest, ndihi, ndilo
     use amr_module, only: ndjhi, ndjlo, store1, store2, storeaux
     use amr_module, only: alloc, hxposs, hyposs
@@ -33,17 +33,10 @@ subroutine flag2refine2(mx,my,mbc,mbuff,meqn,maux,xlower,ylower,dx,dy,t,level, &
 
     use digclaw_module, only: i_h,i_hu,i_hv
 
-    use storm_module, only: storm_specification_type, wind_refine, R_refine
-    use storm_module, only: storm_location, wind_forcing, wind_index, wind_refine
-
     use regions_module, only: num_regions, regions
     use refinement_module, only: wave_tolerance, speed_tolerance
     use refinement_module, only: flowgradevalue, iflowgradevariable, &
         iflowgradetype, iflowgrademinlevel, mflowgrades, keep_fine
-
-    use adjoint_module, only: totnum_adjoints,innerprod_index, &
-                              adjoint_flagging,select_snapshots
-    use adjointsup_module, only: calculate_innerproduct
 
     implicit none
 
@@ -61,16 +54,29 @@ subroutine flag2refine2(mx,my,mbc,mbuff,meqn,maux,xlower,ylower,dx,dy,t,level, &
     integer :: i,j,m,r
     real(kind=8) :: x_c,y_c,x_low,y_low,x_hi,y_hi
     real(kind=8) :: speed, eta, ds
+    logical :: FGFLAG
 
-    !!DIG:  Need to declare more flowgrade/keep_fine variables?
-    real(kind=8) :: flowgradenorm, flowgradegrad, depth, momentum, surface
+    real(kind=8) :: qcen(meqn)
+    real(kind=8) :: qtop(meqn)
+    real(kind=8) :: qbot(meqn)
+    real(kind=8) :: qlef(meqn)
+    real(kind=8) :: qrig(meqn)
+    real(kind=8) :: auxcen(maux)
+    real(kind=8) :: auxtop(maux)
+    real(kind=8) :: auxbot(maux)
+    real(kind=8) :: auxlef(maux)
+    real(kind=8) :: auxrig(maux)
+
+
+    !! Flowgrade and keep_fine variables
+    real(kind=8) :: flowgradenorm, flowgradegrad, depth, velocity, surface_perturbation, momentum, surface
     real(kind=8) :: xlow,xhi,ylow,yhi,xxlow,xxhi,yylow,yyhi
-    real(kind=8) :: flowgrademeasure, h,hu,hv
-    integer :: nx,ny,loc,locaux,mitot,mjtot,mptr,iflow,ii,jj
+    real(kind=8) :: dxfine,dyfine
+    integer :: nx,ny,loc,locaux,mitot,mjtot,mptr,iflow,ii,jj,mm
 
 
     ! Loop over interior points on this grid
-    ! (i,j) grid cell is [x_low,x_hi] x [y_low,y_hi], cell center at (x_c,y_c)
+    ! (i,j) grid cell is [x_low,x_hi] x [y_low,y_hi], cell cen at (x_c,y_c)
     y_loop: do j=1,my
         y_low = ylower + (j - 1) * dy
         y_c = ylower + (j - 0.5d0) * dy
@@ -86,58 +92,35 @@ subroutine flag2refine2(mx,my,mbc,mbuff,meqn,maux,xlower,ylower,dx,dy,t,level, &
                 cycle x_loop
             endif
 
-            ! DIG:  Adapted from D-Claw...
-
             ! determine if flowgrades are used
             if (mflowgrades > 0) then
-                depth= q(i_h,i,j)
-                momentum = sqrt(q(i_hu,i,j)**2 + q(i_hv,i,j)**2)
-                surface = q(i_h,i,j) + aux(1,i,j)
-                do iflow=1,mflowgrades
-                      if (iflowgradevariable(iflow).eq.1) then
-                        flowgradenorm=depth
-                        flowgradegrad=depth
-
-                      elseif (iflowgradevariable(iflow).eq.2) then
-                        flowgradenorm=momentum
-                        flowgradegrad=momentum
-
-                      elseif (iflowgradevariable(iflow).eq.3) then
-
-                        if (depth.gt.dry_tolerance) then
-                          flowgradenorm=dabs(surface-sea_level)
-                          flowgradegrad=dabs(surface-sea_level)
-                        else
-                          flowgradenorm=0.d0
-                          flowgradegrad=0.d0
-                        endif
-                      endif
-
-                      if (iflowgradetype(iflow).eq.1) then
-                        flowgrademeasure=flowgradenorm
-                      else
-                        write(*,*) 'only flowgradetype = 1 supported'
-                        stop
-                        flowgrademeasure=flowgradegrad
-                      endif
-
-                      if (flowgrademeasure.gt.flowgradevalue(iflow) &
-                        .and.level.lt.iflowgrademinlevel(iflow)) then
-                        amrflags(i,j)=DOFLAG
-                        cycle x_loop
-                      endif
-                enddo
-            endif ! mflowgrades > 0
+              call check_flowgrades(meqn,maux,level, &
+                               q(:, i,j), & ! center
+                               q(:, i-1,j), & ! left
+                               q(:, i+1,j), & ! right
+                               q(:, i,j+1), & ! top
+                               q(:, i,j-1), & ! bottom
+                               aux(:, i,j), & ! center
+                               aux(:, i-1,j), & ! left
+                               aux(:, i+1,j), & ! right
+                               aux(:, i,j+1), & ! top
+                               aux(:, i,j-1), & ! bottom
+                               dx,dy,FGFLAG)
+              if (FGFLAG) then
+                amrflags(i,j)=DOFLAG
+                cycle x_loop
+              endif
+            endif
 
             ! keep fine added by KRB 2022/12/28
             if (keep_fine.and.mflowgrades.gt.0) then
-              ! if level is lower than lfine a grid exists here on lfine,
-              ! enforce refinement here.
-              ! here, ignore ghost cells on the fine grid (e.g., no adjustment
-              ! of extent)
+              ! if level is lower than lfine determine whether a grid exists
+              ! here on lfine and test whether refinement should be maintained.
+              ! ignore ghost cells on the fine grid
+
               if (level .lt. lfine) then
 
-                 ! loop through lfine grids (code modified by valout)
+                 ! loop through lfine grids
                  mptr = lstart(lfine)
                  do while (mptr > 0)
 
@@ -187,42 +170,40 @@ subroutine flag2refine2(mx,my,mbc,mbuff,meqn,maux,xlower,ylower,dx,dy,t,level, &
                             ! calculate flowgrade values
                             if (x_hi.gt.xxlow.and.x_low.lt.xxhi.and. &
                                    y_hi.gt.yylow.and.y_low.lt.yyhi) then
-                              h = alloc(iadd(1,ii,jj))
-                              hu = alloc(iadd(2,ii,jj))
-                              hv = alloc(iadd(3,ii, jj))
-                              momentum = sqrt((hu**2)+(hv**2))
-                              surface = h + alloc(iaddaux(1,ii,jj))
 
-                    ! check flowgrade values on fine grid.
-                    do iflow=1,mflowgrades
-                      if (iflowgradevariable(iflow).eq.1) then
-                        flowgradenorm=depth
-                        flowgradegrad=depth
-                      elseif (iflowgradevariable(iflow).eq.2) then
-                        flowgradenorm=momentum
-                        flowgradegrad=momentum
-                      elseif (iflowgradevariable(iflow).eq.3) then
-                        if (depth.gt.dry_tolerance) then
-                          flowgradenorm=dabs(surface-sea_level)
-                          flowgradegrad=dabs(surface-sea_level)
-                        else
-                          flowgradenorm=0.d0
-                          flowgradegrad=0.d0
-                        endif
-                      endif
-                      if (iflowgradetype(iflow).eq.1) then
-                        flowgrademeasure=flowgradenorm
-                      else
-                        write(*,*) 'flowgradetype not supported'
-                        stop
-                        flowgrademeasure=flowgradegrad
-                      endif
-                      if (flowgrademeasure.gt.flowgradevalue(iflow) &
-                          .and.level.lt.iflowgrademinlevel(iflow)) then
-                        amrflags(i,j)=DOFLAG
-                        cycle x_loop
-                      endif
-                    enddo ! end flowgrade loop
+                              ! construct arrays of q and aux for the five cell
+                              ! stencil surrounding cell ii,jj.
+                              do mm = 1, meqn
+                                qcen(mm)=alloc(iadd(mm,ii,jj))
+                                qlef(mm)=alloc(iadd(mm,ii-1,jj))
+                                qrig(mm)=alloc(iadd(mm,ii+1,jj))
+                                qtop(mm)=alloc(iadd(mm,ii,jj+1))
+                                qbot(mm)=alloc(iadd(mm,ii,jj-1))
+                              enddo
+
+                              do mm = i, maux
+                                auxcen(mm)=alloc(iaddaux(mm,ii,jj))
+                                auxlef(mm)=alloc(iaddaux(mm,ii-1,jj))
+                                auxrig(mm)=alloc(iaddaux(mm,ii+1,jj))
+                                auxtop(mm)=alloc(iaddaux(mm,ii,jj+1))
+                                auxbot(mm)=alloc(iaddaux(mm,ii,jj-1))
+                              enddo
+
+                              ! get fine dx and dy values
+                              dxfine=hxposs(lfine)
+                              dyfine=hyposs(lfine)
+
+                              ! call check_flowgrades
+                              call check_flowgrades(meqn,maux,level, &
+                                                    qcen,qlef,qrig,qtop,qbot, &
+                                                    auxcen,auxlef,auxrig,auxtop,auxbot, &
+                                                    dxfine,dyfine, &
+                                                    FGFLAG)
+
+                              if (FGFLAG) then
+                                amrflags(i,j)=DOFLAG
+                                cycle x_loop
+                              endif
 
                             endif ! end if coarse and fine cell overlap
                           enddo ! endif fine cell loop ii
@@ -235,40 +216,6 @@ subroutine flag2refine2(mx,my,mbc,mbuff,meqn,maux,xlower,ylower,dx,dy,t,level, &
               endif ! (end level .lt. lfine)
             endif ! (end if keep_fine)
             ! end keep fine
-
-
-            ! ********* criteria applied only to wet cells:
-
-            if (q(i_h,i,j) > dry_tolerance) then
-
-! DIG: D-claw not yet compatible with adjoint
-!                if(adjoint_flagging) then
-!                    if(aux(innerprod_index,i,j) > tolsp) then
-!                        amrflags(i,j) = DOFLAG
-!                        cycle x_loop
-!                    endif
-!                else
-
-                ! Check wave criteria
-                eta = q(i_h,i,j) + aux(1,i,j)
-                if (abs(eta - sea_level) > wave_tolerance) then
-                    amrflags(i,j) = DOFLAG
-                    cycle x_loop
-                endif
-
-                ! DIG: Speed criteria comparable to flowgrades and keep fine?
-                ! Check speed criteria (distinct values for each level), note that it might be useful to
-                ! also have a per layer criteria since this is not
-                ! gradient based
-                speed = sqrt(q(i_hu,i,j)**2 + q(i_hv,i,j)**2) / q(i_h,i,j)
-                do m=1,min(size(speed_tolerance),mxnest)
-                    if (speed > speed_tolerance(m) .and. level <= m) then
-                        amrflags(i,j) = DOFLAG
-                        cycle x_loop
-                    endif
-                enddo
-!                endif
-            endif
 
 
         enddo x_loop
@@ -291,3 +238,140 @@ contains
     end function iaddaux
 
 end subroutine flag2refine2
+
+subroutine check_flowgrades(meqn,maux,level, &
+                            qcen,qlef,qrig,qtop,qbot, &
+                            auxcen,auxlef,auxrig,auxtop,auxbot, &
+                            dx,dy, &
+                            FGFLAG)
+
+    use refinement_module, only: flowgradevalue, iflowgradevariable, &
+        iflowgradetype, iflowgrademinlevel, mflowgrades
+    use geoclaw_module, only:dry_tolerance, sea_level
+    use digclaw_module, only: i_h,i_hu,i_hv,i_ent
+
+    ! Subroutine arguments
+    integer, intent(in) :: meqn,maux
+
+    real(kind=8), intent(in) :: qcen(meqn)
+    real(kind=8), intent(in) :: qlef(meqn)
+    real(kind=8), intent(in) :: qrig(meqn)
+    real(kind=8), intent(in) :: qtop(meqn)
+    real(kind=8), intent(in) :: qbot(meqn)
+
+    real(kind=8), intent(in) :: auxcen(maux)
+    real(kind=8), intent(in) :: auxlef(maux)
+    real(kind=8), intent(in) :: auxrig(maux)
+    real(kind=8), intent(in) :: auxbot(maux)
+    real(kind=8), intent(in) :: auxtop(maux)
+
+    real(kind=8), intent(in) :: dy, dx
+    logical, intent(inout) :: FGFLAG
+
+    !! Locals
+    real(kind=8) :: flowgrademeasure
+    integer :: iflow
+
+    FGFLAG=.false.
+
+    mfg_loop: do iflow=1,mflowgrades
+
+      ! flowgradevariable = 1, depth
+      if (iflowgradevariable(iflow).eq.1) then
+
+        ! flowgradetype = 1, magnitude
+        if (iflowgradetype(iflow).eq.1) then
+
+          flowgrademeasure=qcen(i_h)
+
+        ! flowgradetype != 1, gradient
+        else
+
+          bot=qbot(i_h)
+          top=qtop(i_h)
+          lef=qlef(i_h)
+          rig=qrig(i_h)
+
+          flowgrademeasure=sqrt(((rig-lef)/(2.0d0*dx))**2+((top-bot)/(2.0d0*dy))**2)
+
+        endif
+
+      ! flowgradevariable = 2, velocity
+      elseif (iflowgradevariable(iflow).eq.2) then
+
+        ! flowgradetype = 1, magnitude
+        if (iflowgradetype(iflow).eq.1) then
+
+          if (qcen(i_h).gt.dry_tolerance) then ! only consider if center cell is wet.
+            flowgrademeasure=sqrt((qcen(i_hu)/qcen(i_h))**2+(qcen(i_hv)/qcen(i_h))**2)
+          else
+            flowgrademeasure=0.d0
+          endif
+
+        ! flowgradetype != 1, gradient
+        else
+
+          if ((qcen(i_h).gt.dry_tolerance).and. &
+              (qtop(i_h).gt.dry_tolerance).and. &
+              (qbot(i_h).gt.dry_tolerance).and. &
+              (qlef(i_h).gt.dry_tolerance).and. &
+              (qrig(i_h).gt.dry_tolerance)) then ! only consider if all stencil cells are wet.
+
+            bot = sqrt((qbot(i_hu)/qbot(i_h))**2 + (qbot(i_hv)/qbot(i_h))**2)
+            top = sqrt((qtop(i_hu)/qtop(i_h))**2 + (qtop(i_hv)/qtop(i_h))**2)
+            lef = sqrt((qlef(i_hu)/qlef(i_h))**2 + (qlef(i_hv)/qlef(i_h))**2)
+            rig = sqrt((qrig(i_hu)/qrig(i_h))**2 + (qrig(i_hv)/qrig(i_h))**2)
+
+            flowgrademeasure=sqrt(((rig-lef)/(2.0d0*dx))**2 + ((top-bot)/(2.0d0*dy))**2)
+          else
+            flowgrademeasure=0.d0
+          endif
+
+        endif
+
+      ! flowgradevariable = 3, perturbation from sea level
+      elseif (iflowgradevariable(iflow).eq.3) then
+
+        ! flowgradetype = 1, magnitude
+        if (iflowgradetype(iflow).eq.1) then
+
+          ! only calculate surface perturbation if center cell is wet.
+          if (qcen(i_h).gt.dry_tolerance) then
+            flowgrademeasure=dabs(qcen(i_h)+auxcen(1)-qcen(i_ent)-sea_level)
+          else
+            flowgrademeasure=0.d0
+          endif
+
+        ! flowgradetype != 1, gradient
+        else
+          if ((qcen(i_h).gt.dry_tolerance).and. &
+              (qtop(i_h).gt.dry_tolerance).and. &
+              (qbot(i_h).gt.dry_tolerance).and. &
+              (qlef(i_h).gt.dry_tolerance).and. &
+              (qrig(i_h).gt.dry_tolerance)) then ! only consider if all stencil cells are wet.
+
+            bot=qbot(i_h)+auxbot(1)-qbot(i_ent)
+            top=qtop(i_h)+auxtop(1)-qtop(i_ent)
+            lef=qlef(i_h)+auxlef(1)-qlef(i_ent)
+            rig=qrig(i_h)+auxrig(1)-qrig(i_ent)
+
+            flowgrademeasure=sqrt(((rig-lef)/(2.0d0*dx))**2+((top-bot)/(2.0d0*dy))**2)
+          else
+            flowgrademeasure=0.d0
+          endif
+        endif
+      else
+        write(*,*) '+++++ only flowgradevariable = 1,2,3 supported'
+        stop
+
+      endif
+
+      if (flowgrademeasure.gt.flowgradevalue(iflow) &
+        .and.level.lt.iflowgrademinlevel(iflow)) then
+        FGFLAG=.true.
+        cycle mfg_loop
+      endif
+
+    enddo mfg_loop! end mflowgrades loop
+
+end subroutine check_flowgrades
